8e9627c165d86d23e091425831bf4f0ceb496b86
Rodrigo Junior Rodrigues <rodrjuniorsantos@gmail.com>
2019-11-27 00:55:16 -0300

    Commit initial

---
+++ src/Projeto.Curso.Core.Pedidos/Entidades/ItemsPedidos.cs
@@ -0,0 +1,12 @@
+﻿namespace Projeto.Curso.Core.Pedidos.Entidades
+{
+    public class ItemsPedidos
+    {
+        public int Id { get; set; }
+        public int Qtd { get; set; }
+        public int IdPedido { get; set; }
+        public int IdProduto { get; set; }
+        public string Apelido { get; set; }
+        public string Nome { get; set; }
+    }
+}
